90a6bfaef44929db29bb4f575b138c3f74721c7b
Georgy Marchuk <gmarcuk@gmail.com>
2019-05-01 00:00:43 +0200

    init

---
+++ src/index.js
@@ -0,0 +1,23 @@
+import Plugin from '@swup/plugin';
+
+export default class GtmPlugin extends Plugin {
+    name = "SwupGtmPlugin";
+
+    mount() {
+        this.swup.on('contentReplaced', event => {
+            if (typeof window.dataLayer === 'object') {
+                const object = {
+                    'event': 'VirtualPageview',
+                    'virtualPageURL': window.location.pathname + window.location.search,
+                    'virtualPageTitle': document.title
+                };
+
+                window.dataLayer.push(object);
+
+                this.swup.log(`GTM pageview (url '${object.virtualPageURL}').`);
+            } else {
+                console.warn('GTM is not loaded.')
+            }
+        })
+    }
+}
